f8179ee0757a0c414b2de2e7ad7bcb8f6596598a
idnoble <id.bellodev@gmail.com>
2025-05-23 17:54:43 -0600

    added deploy k8

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ module "eks" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
@@ -36,5 +36,5 @@ data "aws_eks_cluster_auth" "cluster" {
 
 module "app" {
   source = "./modules/app"
-  deploy_k8s_resources = var.deploy_k8s_resources
+  deploy_k8s_resources = var.deploy_k8s_resourcesommit -m 
 }
